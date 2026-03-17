3c82e9b999dc26065621bed2254e366aeadbb6e8
AndruloyMateo175 <andres@bmwpuntadeleste.com>
2026-03-17 15:00:39 -0300

    add: ruta /import-tool sin syntax error

--- server.py
+++ server.py
@@ -1,3 +1,14 @@
+        if self.path == '/import-tool':
+            try:
+                with open('import_tool.html', 'rb') as f:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html; charset=utf-8')
+                    self.end_headers()
+                    self.wfile.write(f.read())
+            except:
+                self.send_response(404)
+                self.end_headers()
+            return
 #!/usr/bin/env python3
 """
 AutomotoraGV - Sistema de Gestion de Vehiculos
